c8b1e0db56264b47926bf8bbd07414cb2efd342d
Callum McLoughlin <59436970+CallumMcLoughlin@users.noreply.github.com>
2020-08-15 16:54:30 +1200

    Initial commit

---
+++ src/main/java/MazeSearcher/App.java
@@ -0,0 +1,13 @@
+package MazeSearcher;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
